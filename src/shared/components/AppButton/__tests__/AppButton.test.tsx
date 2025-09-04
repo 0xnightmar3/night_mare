7923e5c710cb54590449857ab141b6f0c8b67b2f
Glitch <themar3machine@gmail.com>
2025-09-04 13:53:11 +0200

    Format code further

--- src/shared/components/AppButton/__tests__/AppButton.test.tsx
+++ src/shared/components/AppButton/__tests__/AppButton.test.tsx
@@ -8,55 +8,56 @@ import AppButton, { type IProps } from "../AppButton";
 const TEXT = "Hello World!";
 
 const defaultProps: IProps = {
-    text: TEXT,
-    style: ButtonType.dark,
-    clickHandler: vi.fn(),
+  text: TEXT,
+  style: ButtonType.dark,
+  clickHandler: vi.fn(),
 };
 
 const setup = (override: Partial<IProps> = {}) => {
-    const clickFn = vi.fn();
-    const props = { ...defaultProps, clickHandler: clickFn, ...override };
-    const user = userEvent.setup();
-    render(<AppButton {...props} />);
-    const btn = screen.getByRole("button");
+  const clickFn = vi.fn();
+  const props = { ...defaultProps, clickHandler: clickFn, ...override };
+  const user = userEvent.setup();
+  render(<AppButton {...props} />);
+  const btn = screen.getByRole("button");
 
-    return { clickFn, user, btn, };
+  return { clickFn, user, btn };
 };
 
 describe("<AppButton />", () => {
-    it("renders a button with given text", () => {
-        const { btn } = setup();
-        expect(btn).toBeInTheDocument();
-        expect(btn).toHaveTextContent(TEXT);
-    });
+  it("renders a button with given text", () => {
+    const { btn } = setup();
+    const textRegex = new RegExp(`^${TEXT}$`);
+    expect(btn).toBeInTheDocument();
+    expect(btn).toHaveTextContent(textRegex);
+  });
 
-    it("applies the provided style class", () => {
-        const { btn } = setup();
-        expect(btn).toHaveClass(ButtonType.dark);
-        expect(btn).not.toHaveClass(ButtonType.light);
-        expect(btn).toHaveClass("app-button");
-    });
+  it("applies the provided style class", () => {
+    const { btn } = setup();
+    expect(btn).toHaveClass(ButtonType.dark);
+    expect(btn).not.toHaveClass(ButtonType.light);
+    expect(btn).toHaveClass("app-button");
+  });
 
-    it("calls click handler on mouse click", async () => {
-        const { clickFn, user, btn } = setup();
-        expect(clickFn).not.toBeCalled();
-        await user.click(btn);
-        expect(clickFn).toBeCalledTimes(1);
-    });
+  it("calls click handler on mouse click", async () => {
+    const { clickFn, user, btn } = setup();
+    expect(clickFn).not.toBeCalled();
+    await user.click(btn);
+    expect(clickFn).toBeCalledTimes(1);
+  });
 
-    it("is focusable for accessibility", () => {
-        const { btn } = setup();
-        btn.focus();
-        expect(btn).toHaveFocus();
-    });
+  it("is focusable for accessibility", () => {
+    const { btn } = setup();
+    btn.focus();
+    expect(btn).toHaveFocus();
+  });
 
-    it("is clickable by enter or space for accessibility", async () => {
-        const { btn, user, clickFn } = setup();
-        btn.focus();
+  it("is clickable by enter or space for accessibility", async () => {
+    const { btn, user, clickFn } = setup();
+    btn.focus();
 
-        await user.keyboard("{Enter}");
-        await user.keyboard(" ");
+    await user.keyboard("{Enter}");
+    await user.keyboard(" ");
 
-        expect(clickFn).toHaveBeenCalledTimes(2);
-    });
+    expect(clickFn).toHaveBeenCalledTimes(2);
+  });
 });
